dc0fd92c50e7eda6252e614907767a46ee95381e
Enrico Martinez <enricobaroni@hotmail.es>
2026-05-17 17:55:34 +0200

    Folder new organisation
    Added and removed folders to reorder to a more correct structure

--- scripts/dungeon_room.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	$PlayerDetector.body_entered.connect(_on_player_detector_body_entered)
-
-func _on_player_detector_body_entered(body: Node2D) -> void:
-	Events.room_entered.emit(self)
