43d119b8348115ca3d2ec30e466b389c975e01ea
Enrico Martinez <enricobaroni@hotmail.es>
2026-08-25 00:31:01 +0200

    Changed the way grass works so it can be similar to poop in tboi

--- world/grass.gd
+++ world/grass.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends StaticBody2D
 
 @export var GRASS_EFFECT: PackedScene
 
@@ -8,6 +8,9 @@ func _ready() -> void:
 	hurtbox.hurt.connect(_on_hurt)
 
 func _on_hurt(other_hitbox: Hitbox) -> void:
+	if other_hitbox.source_faction != "player":
+		return
+
 	var grass_effect_instance = GRASS_EFFECT.instantiate()
 	grass_effect_instance.global_position = global_position
 	get_tree().current_scene.add_child(grass_effect_instance)
